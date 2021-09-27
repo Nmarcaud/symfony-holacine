82370c864aa561af5611a685a643003a2271085b
Nicolas Marcaud <nicolasmarcaud@MacBook-Air-de-Nicolas-2.local>
2021-09-27 17:15:47 +0200

    Before Crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Config\Doctrine\Orm\EntityManagerConfig\EntityListenersConfig;
 
 class HomeController extends AbstractController
 {
+
+    private EntityManagerInterface $em;
+
+	public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
+        $films = $this->em->getRepository(Film::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'films' => $films
         ]);
     }
 }
